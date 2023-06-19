c1460df825142677780df2e96fa32709e5423d14
lorenzoSanchez97 <lorenzo97sz@gmail.com>
2023-06-18 21:42:09 -0300

    Mejor animaciones nav-bar

--- public/js/desplegable.js
+++ public/js/desplegable.js
@@ -3,10 +3,12 @@ let listaDesplegable = document.querySelectorAll(".lista-desplegable")
 
 for (let i = 0; i < desplegable.length; i++) {
     desplegable[i].addEventListener("mouseenter", () => {
-    listaDesplegable[i].classList.add("lista-desplegable-mostrar")
+        listaDesplegable[i].classList.add("lista-desplegable-mostrar")
+        listaDesplegable[i].classList.remove("lista-desplegable-ocultar")
     })
-    
+
     desplegable[i].addEventListener("mouseleave", () => {
-    listaDesplegable[i].classList.remove("lista-desplegable-mostrar")
+        listaDesplegable[i].classList.add("lista-desplegable-ocultar")
+        listaDesplegable[i].classList.remove("lista-desplegable-mostrar")
     })
 }
