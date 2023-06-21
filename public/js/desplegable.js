8ea3ecedfaa20248a40f3a13e887a97338a7881a
lorenzoSanchez97 <lorenzo97sz@gmail.com>
2023-06-21 00:14:28 -0300

    Ajusto iconos

--- public/js/desplegable.js
+++ public/js/desplegable.js
@@ -1,14 +1,16 @@
-let desplegable = document.querySelectorAll(".desplegable")
-let listaDesplegable = document.querySelectorAll(".lista-desplegable")
+window.addEventListener("load", () => {
+    let desplegable = document.querySelectorAll(".desplegable")
+    let listaDesplegable = document.querySelectorAll(".lista-desplegable")
 
-for (let i = 0; i < desplegable.length; i++) {
-    desplegable[i].addEventListener("mouseenter", () => {
-        listaDesplegable[i].classList.add("lista-desplegable-mostrar")
-        listaDesplegable[i].classList.remove("lista-desplegable-ocultar")
-    })
+    for (let i = 0; i < desplegable.length; i++) {
+        desplegable[i].addEventListener("mouseenter", () => {
+            listaDesplegable[i].classList.add("lista-desplegable-mostrar")
+            listaDesplegable[i].classList.remove("lista-desplegable-ocultar")
+        })
 
-    desplegable[i].addEventListener("mouseleave", () => {
-        listaDesplegable[i].classList.add("lista-desplegable-ocultar")
-        listaDesplegable[i].classList.remove("lista-desplegable-mostrar")
-    })
-}
+        desplegable[i].addEventListener("mouseleave", () => {
+            listaDesplegable[i].classList.add("lista-desplegable-ocultar")
+            listaDesplegable[i].classList.remove("lista-desplegable-mostrar")
+        })
+    }
+})
