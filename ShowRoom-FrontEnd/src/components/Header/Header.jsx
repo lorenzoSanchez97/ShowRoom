9bf67393e55ef7b916764558aa67f48a1d5529e9
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-06 18:19:23 -0300

    Ajusto efectos header

--- ShowRoom-FrontEnd/src/components/Header/Header.jsx
+++ ShowRoom-FrontEnd/src/components/Header/Header.jsx
@@ -1,51 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./header.css";
 
 function Header() {
+
+    let [mouseEvent, setMouseEvent] = React.useState(2)
+
+    const handleMouseLeave = (e) => {
+        const relatedTarget = e.relatedTarget;
+        const isChildElement = relatedTarget && relatedTarget.closest("li");
+    
+        if (!isChildElement) {
+          setMouseEvent(0);
+        }
+      };
+
     return (
         <header>
-    <div className="cat">
-        <img src="/images/Cat.png" alt=""/>
-    </div>
-    <section className="header-top-bar">
-        <div>
-            <h1>LOGO</h1>
-        </div>
-        <form className="search-bar" action="" method="GET">
-            <input type="text" placeholder="Buscar productos"/>
-            <button><i className="fa fa-search"></i></button>
-        </form>
-        <div>
-            <a className="sesion" href="">Iniciar sesión <i className="fa-solid fa-arrow-right-to-bracket"></i></a>
-        </div>
-        <div>
-            <a className="carrito" href="/cart">Carrito <i className="fa-solid fa-cart-shopping"></i></a>         
-        </div>
-    </section>
-    <nav className="nav-bar">
-        <ul className="nav-bar-list">
-            <li className="nav-bar-category"><a href="/">Inicio</a></li>
-            <li className="nav-bar-category desplegable"><a href="">Productos</a>
-                <ul className="lista-desplegable">
-                    <li className="subcategoria"><a href="">Mascarillas</a></li>
-                    <li className="subcategoria"><a href="">Depilación</a></li>
-                    <li className="subcategoria"><a href="">Limpieza</a></li>
-                    <li className="subcategoria"><a href="">Maquillaje</a></li>
-                    <li className="subcategoria"><a href="">Accesorios</a></li>
+            <div className="cat">
+                <img src="/images/Cat.png" alt="" />
+            </div>
+            <section className="header-top-bar">
+                <div>
+                    <h1>LOGO</h1>
+                </div>
+                <form className="search-bar" action="" method="GET">
+                    <input type="text" placeholder="Buscar productos" />
+                    <button><i className="fa fa-search"></i></button>
+                </form>
+                <div>
+                    <a className="sesion" href="">Iniciar sesión <i className="fa-solid fa-arrow-right-to-bracket"></i></a>
+                </div>
+                <div>
+                    <a className="carrito" href="/cart">Carrito <i className="fa-solid fa-cart-shopping"></i></a>
+                </div>
+            </section>
+            <nav className="nav-bar">
+                <ul className="nav-bar-list">
+                    <li className="nav-bar-category"><a href="/">Inicio</a></li>
+                    <li className="nav-bar-category desplegable" onMouseEnter={() => setMouseEvent(1)} onMouseLeave={handleMouseLeave}><a href="">Productos</a>
+                        <ul className={`lista-desplegable ${mouseEvent === 1 ? "lista-desplegable-mostrar" : 
+                                        mouseEvent === 0 ? "lista-desplegable-ocultar" : ""}`}>
+                            <li className="subcategoria"><a href="">Mascarillas</a></li>
+                            <li className="subcategoria"><a href="">Depilación</a></li>
+                            <li className="subcategoria"><a href="">Limpieza</a></li>
+                            <li className="subcategoria"><a href="">Maquillaje</a></li>
+                            <li className="subcategoria"><a href="">Accesorios</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-bar-category"><a href="">Ofertas</a></li>
+                    <li className="nav-bar-category"><a href="">Novedades</a></li>
+                    <li className="nav-bar-category desplegable"><a href="">Información</a>
+                        <ul className="lista-desplegable">
+                            <li className="subcategoria"><a href="">Cómo comprar</a></li>
+                            <li className="subcategoria"><a href="">Envíos</a></li>
+                            <li className="subcategoria"><a href="">Sobre nosotras</a></li>
+                        </ul>
+                    </li>
                 </ul>
-            </li>
-            <li className="nav-bar-category"><a href="">Ofertas</a></li>
-            <li className="nav-bar-category"><a href="">Novedades</a></li>
-            <li className="nav-bar-category desplegable"><a href="">Información</a>
-                <ul className="lista-desplegable">
-                    <li className="subcategoria"><a href="">Cómo comprar</a></li>
-                    <li className="subcategoria"><a href="">Envíos</a></li>
-                    <li className="subcategoria"><a href="">Sobre nosotras</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-</header>
+            </nav>
+        </header>
     )
 }
 
