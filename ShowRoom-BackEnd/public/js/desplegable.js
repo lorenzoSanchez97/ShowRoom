8d423eec48554af61954009ab813b4f25b9dd498
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-08 20:51:54 -0300

    muevo scripts al back

---
+++ ShowRoom-BackEnd/public/js/desplegable.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", () => {
+    let desplegable = document.querySelectorAll(".desplegable")
+    let listaDesplegable = document.querySelectorAll(".lista-desplegable")
+
+    for (let i = 0; i < desplegable.length; i++) {
+        desplegable[i].addEventListener("mouseenter", () => {
+            listaDesplegable[i].classList.add("lista-desplegable-mostrar")
+            listaDesplegable[i].classList.remove("lista-desplegable-ocultar")
+        })
+
+        desplegable[i].addEventListener("mouseleave", () => {
+            listaDesplegable[i].classList.add("lista-desplegable-ocultar")
+            listaDesplegable[i].classList.remove("lista-desplegable-mostrar")
+        })
+    }
+})
