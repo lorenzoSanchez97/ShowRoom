75b7490ee711aaae9b2fcb2c9e90c4c5fe1c942e
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-09 16:44:58 -0300

    Creo componente modal

---
+++ ShowRoom-FrontEnd/src/components/Modal/Modal.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./modal.css"
+
+function Modal() {
+    
+    return (
+        <>
+        <section className="modal-main-container">
+            <div className="register">
+                <h2>REGISTRATE</h2>
+            </div>
+            <div className="login">
+                <h2>INICIA SESIÓN</h2>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Modal
