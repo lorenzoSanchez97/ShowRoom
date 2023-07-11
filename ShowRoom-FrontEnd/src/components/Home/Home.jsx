8f53290ecd0ddd1413f9182956e11e7f15251f3f
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-10 23:44:18 -0300

    Ajusto z-index

--- ShowRoom-FrontEnd/src/components/Home/Home.jsx
+++ ShowRoom-FrontEnd/src/components/Home/Home.jsx
@@ -4,67 +4,67 @@ import "./home.css"
 function Home() {
 
     return (
-        <body>
-        <div id="banner">
-            <img src="/images/Banner.png" alt=""/>
-        </div>
-        <main>
+        <section className="body-section">
+            <div id="banner">
+                <img src="/images/Banner.png" alt="" />
+            </div>
+            <main>
                 <h1 className="title">Productos en oferta</h1>
                 <section className="seccion-principal">
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                     <article className="producto">
-                        <img src="/images/mascarilla.png" alt=""/>
+                        <img src="/images/mascarilla.png" alt="" />
                         <p className="nombre-producto">Mascarilla de Coco</p>
                         <p className="precio-producto">$999</p>
                         <p className="descuento-producto">$10</p>
                     </article>
                 </section>
-        </main>
+            </main>
 
-    </body>
-    
-)
+        </section>
+
+    )
 }
 
 export default Home;
