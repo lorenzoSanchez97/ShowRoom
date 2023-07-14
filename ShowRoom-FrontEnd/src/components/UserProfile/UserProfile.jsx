7d012370a6c8fb04b49841aef34e722432bc7fe0
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-14 19:08:19 -0300

    Perfil: información personal lista

--- ShowRoom-FrontEnd/src/components/UserProfile/UserProfile.jsx
+++ ShowRoom-FrontEnd/src/components/UserProfile/UserProfile.jsx
@@ -10,7 +10,22 @@ export default function UserProfile() {
             <img src="/images/profile-image.jpeg" alt="" />
           </div>
           <div className="user-data-container">
-          <h2 className="user-data-title">Información personal</h2>
+            <div className="user-info-title-and-button">
+              <h2 className="user-data-title">Información personal</h2>
+              <button><i class="fa-solid fa-pen-to-square"></i></button>
+            </div>
+            <div className="user-info-item-container">
+              <p className="user-info-subtitle">Nombre y apellido</p>
+              <p>Lorenzo Sanchez</p>
+            </div>
+            <div className="user-info-item-container">
+              <p className="user-info-subtitle">Número de teléfono</p>
+              <p>+542946584612</p>
+            </div>
+            <div className="user-info-item-container">
+              <p className="user-info-subtitle">Email</p>
+              <p>lorenzo.sanchez@gmail.com</p>
+            </div>
           </div>
         </div>
         <div className="datos-envio-container">
