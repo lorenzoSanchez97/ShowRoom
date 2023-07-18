bc26543b4956451f0fc08c43737ce4c54689568a
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-18 14:03:27 -0300

    Agrego botón para cargar datos de envio desde el perfil

--- ShowRoom-FrontEnd/src/components/Card/Card.jsx
+++ ShowRoom-FrontEnd/src/components/Card/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./card.css"
 
-export default function Card({ title, data }) {
+export default function Card({ title, data, width }) {
+
+    const containerStyle = width ? { width: width } : {};
+
     return (
-        <div className='card-main-container'>
+        <div className='card-main-container' style={containerStyle}>
             <p className='card-title'>{title}</p>
-            <p className="card-data">{data}</p>
-        </div>
+            {typeof data !== "string" && typeof data !== "number" ? data : <p className="card-data">{data}</p>}
+        </div >
 
     )
 }
--- ShowRoom-FrontEnd/src/components/UserProfile/UserProfile.jsx
+++ ShowRoom-FrontEnd/src/components/UserProfile/UserProfile.jsx
@@ -5,6 +5,14 @@ import Tabla from "../Tabla/Tabla";
 import Card from "../Card/Card";
 
 export default function UserProfile() {
+
+  let [infoEnvio, setInfoEnvio] = React.useState(null)
+
+  // provincia: "Córdoba",
+  //   ciudad: "Rio Cuarto",
+  //   códigoPostal: "6587",
+  //   dirección: "Tosco 123"
+
   return (
     <>
       <main className="user-profile-main-container">
@@ -16,27 +24,37 @@ export default function UserProfile() {
             title="Información personal"
             subtitles={["Nombre y apellido", "Número de teléfono", "Email"]}
             data={["Lorenzo Sanchez", "+54287962189", "lorenzo.sanchez@gmail.com"]} />
-          <InfoBox
-            title="Información de envío"
-            subtitles={["Provincia", "Ciudad", "Código postal", "Dirección"]}
-            data={["Córdoba", "Rio Cuarto", "6587", "Tosco 123"]} />
+          {infoEnvio ?
+            <InfoBox
+              title="Información de envío"
+              subtitles={["Provincia", "Ciudad", "Código postal", "Dirección"]}
+              data={["Córdoba", "Rio Cuarto", "6587", "Tosco 123"]} />
+            :
+            <div className="no-data-envio">
+              <Card
+                title={"Aún no ha cargado su información de envío"}
+                data={<button><i class="fa-solid fa-plus"></i></button>}
+                width={"100%"}
+              />
+            </div>
+          }
         </section>
         <section className="historial-compras">
           <div className="historial-header">
             <h1>Historial de compras</h1>
             <div className="historial-header-cards-container">
-            <Card 
-            title={"Total de productos comprados:"}
-            data={"3"}
-            />
-            <Card 
-            title={"Fecha de última compra:"}
-            data={"15/7/2023"}
-            />
-            <Card 
-            title={"Último producto comprado:"}
-            data={"Mascarilla de coco"}
-            />
+              <Card
+                title={"Total de productos comprados:"}
+                data={"3"}
+              />
+              <Card
+                title={"Fecha de última compra:"}
+                data={"15/7/2023"}
+              />
+              <Card
+                title={"Último producto comprado:"}
+                data={"Mascarilla de coco"}
+              />
             </div>
           </div>
           <Tabla
