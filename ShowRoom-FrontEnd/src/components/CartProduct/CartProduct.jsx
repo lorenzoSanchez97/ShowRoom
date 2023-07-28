fa279578fec67763bb24f06b3baa0a47c21a0268
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-28 12:15:32 -0300

    Avanzo con estilos de cart product

--- ShowRoom-FrontEnd/src/components/CartProduct/CartProduct.jsx
+++ ShowRoom-FrontEnd/src/components/CartProduct/CartProduct.jsx
@@ -3,20 +3,24 @@ import "./cartProduct.css"
 
 export default function CartProduct() {
     return (
-        <article className='cart-product-container'>
+        <section className='cart-product-container'>
             <figure className='cart-product-image-container'>
                 <img src="/images/mascarilla.png" alt="" />
             </figure>
             <div className='cart-product-data-container'>
-            <h1 className='cart-product-title'>Mascarilla de Coco</h1>
-            <p className='cart-product-color'>Color: Marrón</p>
-            <button className='cart-product-delete-button'>Eliminar</button>
+                <h1 className='cart-product-title'>Mascarilla de Coco</h1>
+                <p className='cart-product-color'>Color: Marrón</p>
+                <button className='cart-product-delete-button'>Eliminar</button>
             </div>
             <div className='product-cart-quantity-selector'>
                 <form action="">
-                    <input type="number" defaultValue={1} />
+                    <i class="fa-solid fa-plus cart-product-add"></i>
+                    <input type="number" />
+                    <p className='cart-product-stock'>Stock: 25 u.</p>
+                    <i class="fa-solid fa-minus cart-product-substract"></i>
                 </form>
             </div>
-        </article>
+            <p className='cart-product-total'>$2215</p>
+        </section>
     )
 }
