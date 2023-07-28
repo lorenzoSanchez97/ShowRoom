987710abe5cad795b3d001f2bc8c6a850468f48b
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-28 16:30:03 -0300

    Product cart listo

--- ShowRoom-FrontEnd/src/components/CartProduct/CartProduct.jsx
+++ ShowRoom-FrontEnd/src/components/CartProduct/CartProduct.jsx
@@ -3,24 +3,32 @@ import "./cartProduct.css"
 
 export default function CartProduct() {
     return (
-        <section className='cart-product-container'>
+        <section className='cart-product-main-container'>
             <figure className='cart-product-image-container'>
                 <img src="/images/mascarilla.png" alt="" />
             </figure>
-            <div className='cart-product-data-container'>
-                <h1 className='cart-product-title'>Mascarilla de Coco</h1>
-                <p className='cart-product-color'>Color: Marrón</p>
-                <button className='cart-product-delete-button'>Eliminar</button>
+            <div className='product-cart-info-main-container'>
+                <div className='cart-product-info'>
+                    <div className='cart-product-data-container'>
+                        <h1 className='cart-product-title'>Mascarilla de Coco</h1>
+                        <p className='cart-product-color'>Color: Marrón</p>
+                        <button className='cart-product-delete-button'>Eliminar</button>
+                    </div>
+                    <div className='product-cart-quantity-selector'>
+                        <form action="">
+                            <i class="fa-solid fa-plus cart-product-add"></i>
+                            <input type="number" />
+                            <p className='cart-product-stock'>Stock: 25 u.</p>
+                            <i class="fa-solid fa-minus cart-product-substract"></i>
+                        </form>
+                    </div>
+                    <p className='cart-product-total'>$2215</p>
+                </div>
+                <div className='cart-product-envio-data'>
+                    <h1>Costo de envío: </h1>
+                    <p>$1000</p>
+                </div>
             </div>
-            <div className='product-cart-quantity-selector'>
-                <form action="">
-                    <i class="fa-solid fa-plus cart-product-add"></i>
-                    <input type="number" />
-                    <p className='cart-product-stock'>Stock: 25 u.</p>
-                    <i class="fa-solid fa-minus cart-product-substract"></i>
-                </form>
-            </div>
-            <p className='cart-product-total'>$2215</p>
         </section>
     )
 }
