9b531782524b91aca5b8be8fcb3e4565e43cfe3c
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2023-07-29 16:20:56 -0300

    Agrego sombras

--- ShowRoom-FrontEnd/src/components/ProductImages/ProductImages.jsx
+++ ShowRoom-FrontEnd/src/components/ProductImages/ProductImages.jsx
@@ -7,18 +7,40 @@ function ProductImages() {
     let images = ["mascarilla.png", "mascarilla2.jpg", "mascarilla3.jpg", "mascarilla4.jpg"]
 
     let [slide, setSlide] = React.useState(0)
+    let [posicionInicial, setPosicionInicial] = React.useState(0);
+    let [desplazamiento, setDesplazamiento] = React.useState(0);
 
     const handlePrevButton = () => {
-    setSlide (slide - 1)
+        setSlide(slide - 1)
     }
 
     const handleNextButton = () => {
-        setSlide (slide + 1)
+        setSlide(slide + 1)
     }
 
     const handleMouseEnter = (index) => {
         setSlide(index)
     }
+
+    const handleTouchStart = (e) => {
+        setPosicionInicial(e.touches[0].clientX);
+    };
+
+    const handleTouchMove = (e) => {
+        setDesplazamiento(e.touches[0].clientX - posicionInicial);
+    };
+
+    const handleTouchEnd = () => {
+        if (Math.abs(desplazamiento) > 50) {
+            if (desplazamiento > 0) {
+                handlePrevButton();
+            } else {
+                handleNextButton();
+            }
+        }
+        setDesplazamiento(0);
+    };
+
 
     React.useEffect(() => {
         if (images.length > 0) {
@@ -32,18 +54,25 @@ function ProductImages() {
     }, [slide])
 
     return (
-        <div className="product-images-main-container">
+
+        <div className="product-images-main-container"
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}>
             <div className="mini-imagenes-container">
-                {images.map ((img, index) => {
-                   return <img key={index} className={`slide-mini ${index == slide ? "mini-imagenes-hover" : ""}`} onMouseEnter={() => handleMouseEnter(index)} src={`/images/${img}`} alt="" />
+                {images.map((img, index) => {
+                    return <img key={index} className={`slide-mini ${index == slide ? "mini-imagenes-hover" : ""}`} onMouseEnter={() => handleMouseEnter(index)} src={`/images/${img}`} alt="" />
                 })}
             </div>
             <figure className="imagen-producto">
+                <div className="images-counter">
+                    <p>{slide + 1} / {images.length}</p>
+                </div>
                 <img className="mainSlide" src={`/images/${images[slide]}`} alt="" />
-            <div className="botones">
-                    <button className="image-button prev" onClick={handlePrevButton}><i className="fa-solid fa-arrow-left"></i></button>
-                    <button className="image-button next" onClick={handleNextButton}><i className="fa-solid fa-arrow-right"></i></button>
-            </div>
+                <div className="botones">
+                    <button className="image-button prev" onClick={handlePrevButton}><i class="fa-solid fa-angle-left"></i></button>
+                    <button className="image-button next" onClick={handleNextButton}><i class="fa-solid fa-angle-right"></i></button>
+                </div>
             </figure>
         </div>
     )
